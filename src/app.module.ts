3abbc4488b30d23ba68f2f9fdcefacc73eaf0664
Marcos Esteban <83374363+MarcosEstebanDev@users.noreply.github.com>
2024-12-27 16:41:13 -0300

    Update

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { \[testController\]Controller } from './[test-controller/]/[test-controller/].controller';
+import { NameController } from './name/name.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, \[testController\]Controller, NameController],
   providers: [AppService],
 })
 export class AppModule {}
